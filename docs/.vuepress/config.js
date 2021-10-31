2aaadd3a295be555d842cec148383729d66403f5
jinhang <jinhang87@163.com>
2021-10-31 10:47:49 +0800

    seo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { en } = require("./configs/navbar/en");
 
 module.exports = {
-  base: "/vuepress-plugin-tailwind/",
+  //base: "/vuepress-plugin-tailwind/",
   title: name,
   description,
   head: [
